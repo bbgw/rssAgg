d67254215842aabd25c63135c9d9258ed605094c
kangzhao <kangzhao@140-182-233-207.dhcp-bl.indiana.edu>
2012-06-25 09:44:25 -0400

    rss aggregator

--- RssAggregator.php
+++ RssAggregator.php
@@ -1,44 +1,45 @@
 <?php
+
 include('XML2Array.php');
 
-function getFeed($feed_url) {  
-  
-    $content = file_get_contents($feed_url);  
-    $x = new SimpleXMLElement($content);  
+function getFeed($feed_url) {
 
-$dom = new DOMDocument();
-$dom->loadXML($x->asXML());
+    $content = file_get_contents($feed_url);
+    $x = new SimpleXMLElement($content);
 
- $xml_array = XML2Array::createArray($dom);
+    $dom = new DOMDocument();
+    $dom->loadXML($x->asXML());
 
-foreach($xml_array['feed']['entry'] as $item ){
-$title = $item['title']['@cdata'];
-$link = $item['link']['0']['@attributes']['href'];
-$content = $item['summary']['@cdata'];
-$content = strip_tags($content);
-echo $title . "\n" . $link . "\n" . $content . "\n\n";
-}
+    $xml_array = XML2Array::createArray($dom);
+
+    foreach ($xml_array['feed']['entry'] as $item) {
+        $title = $item['title']['@cdata'];
+        $link = $item['link']['0']['@attributes']['href'];
+        $content = $item['summary']['@cdata'];
+        $content = strip_tags($content);
+        echo $title . "\n" . $link . "\n" . $content . "\n\n";
+    }
 //print_r($xml_array); 
-   // echo "<ul>";  
-  
+    // echo "<ul>";  
 //    foreach($x->channel->item as $entry) {  
- //       echo "<li><a href='$entry->link' title='$entry->title'>" . $entry->title . "</a></li>";  
-  //  }  
+    //       echo "<li><a href='$entry->link' title='$entry->title'>" . $entry->title . "</a></li>";  
+    //  }  
     //echo "</ul>";  
 }
+
 $url = 'http://rss1.smashingmagazine.com/feed/';
 getFeed($url);
 /*
-$ch = curl_init("http://rss1.smashingmagazine.com/feed/");
-//$fp = fopen("example_homepage.txt", "w");
+  $ch = curl_init("http://rss1.smashingmagazine.com/feed/");
+  //$fp = fopen("example_homepage.txt", "w");
 
-//curl_setopt($ch, CURLOPT_FILE, $fp);
-//curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  //curl_setopt($ch, CURLOPT_FILE, $fp);
+  //curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-$return = curl_exec($ch);
-print $result;
-curl_close($ch);
-//fclose($fp);
-*/
+  $return = curl_exec($ch);
+  print $result;
+  curl_close($ch);
+  //fclose($fp);
+ */
 ?>
